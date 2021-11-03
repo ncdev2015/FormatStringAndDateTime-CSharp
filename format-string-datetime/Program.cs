ee80a8a8674de7097de7732a0bf3f9cd0b8fab89
ncdev2015 <ncdev2015@gmail.com>
2021-11-03 12:12:16 -0300

    Revert "Update comments description"
    This reverts commit 91c31ca2a39b5ffa189cf37150c3202ad9b49495.

--- format-string-datetime/Program.cs
+++ format-string-datetime/Program.cs
@@ -9,8 +9,8 @@ namespace format_string_datetime
         {
             // Get a Default DateTime value
             DateTime dateTime = new DateTime();
-            Console.WriteLine("dateTime: " + dateTime); // ->  1/1/0001 12:00:00 a. m.
-            Console.WriteLine("dateTime (dd/MM/yyyy): " + dateTime.ToString("dd/MM/yyyy")); // -> 01/01/0001
+            Console.WriteLine("dateTime: " + dateTime);
+            Console.WriteLine("dateTime (dd/MM/yyyy):" + dateTime.ToString("dd/MM/yyyy"));
 
             Console.WriteLine();
 
@@ -18,14 +18,14 @@ namespace format_string_datetime
             string dateTimeStringInput = "2020-08-02T06:00:00.000Z";
             DateTime dateTimeInput = DateTime.Parse(dateTimeStringInput, new CultureInfo("es-AR"));
 
-            Console.WriteLine("dateTimeInput (String): " + dateTimeStringInput); // -> dateTimeInput(String): 2020 - 08 - 02T06: 00:00.000Z
-            Console.WriteLine("dateTimeInput (DateTime): " + dateTimeInput); // -> dateTimeInput (DateTime): 2/8/2020 3:00:00 a. m.
+            Console.WriteLine("dateTimeInput (String): " + dateTimeStringInput);
+            Console.WriteLine("dateTimeInput (DateTime): " + dateTimeInput);
 
             Console.WriteLine();
 
             // Creates a string output by DateTime values
             string output = dateTimeInput.ToString("dd/MM/yyyy") + " (" + dateTimeInput.ToString("MMMM").ToUpper().Substring(0, 3) + " " + dateTimeInput.ToString("yy") + ")";
-            Console.WriteLine(output); // -> 02/08/2020 (AGO 20)
+            Console.WriteLine(output);
         }
     }
 }
